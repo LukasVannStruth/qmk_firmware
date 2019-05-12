1a52c89a83ce68ccf92b715e340240e356a36d1c
Lukas Vann Struth <lvannstruth@gmail.com>
2019-05-12 13:22:50 -0700

    layout updated and preparing for merge

--- keyboards/atreus/keymaps/lukasvannstruth/keymap.c
+++ keyboards/atreus/keymaps/lukasvannstruth/keymap.c
@@ -12,13 +12,13 @@
 #define _FUNC 2
 
 #define TAPPING_TOGGLE 2
-
+// remember on my atreus the central column is upside down so alt is on the left and control is on the left
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_TYPING] = LAYOUT( /* Qwerty */
     KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                      KC_Y,    KC_U,    KC_I,    KC_O,    KC_P    ,
     KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                      KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN ,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH ,
-    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LALT, KC_LCTRL, KC_SPC,  TT(_NUMNAV), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_TAB, KC_LGUI,  KC_LSFT, KC_BSPC,  KC_LALT, KC_LCTRL, KC_SPC,  MO(_NUMNAV), KC_MINS, KC_QUOT, KC_ENT
   ),
 /*
   // old numnav layout
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /*
    *  -      7     8     9     {         ||         }     home  pgdn  pgup end
    *  =      4     5     6     [         ||         ]     left  down  up   rght
-   *  `      1     2     3     (         ||         )     "     psng  p\ps nsng
-   *  func   0     lgui  shft  del  ctrl || alt     spce  num   vold  musc volu
+   *  `      1     2     3     (         ||         )     \     psng  p\ps nsng
+   *  func   0     lgui  shft  bspc ctrl || alt     spce  num   vold  musc volu
    */
   [_NUMNAV] = LAYOUT( /* [> RAISE <] */
     KC_MINS,   KC_7,   KC_8,    KC_9, KC_LCBR,                   KC_RCBR, KC_HOME, KC_PGDN, KC_PGUP, KC_END,
     KC_EQL,    KC_4,   KC_5,    KC_6, KC_LBRC,                   KC_RBRC, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
     KC_GRV,    KC_1,   KC_2,    KC_3, KC_LPRN,                   KC_RPRN, KC_BSLS, KC_MPRV, KC_MPLY, KC_MNXT,
-    TT(_FUNC), KC_0, KC_LGUI, KC_LSFT, KC_DEL, KC_LCTL, KC_LALT, KC_SPC,  KC_TRNS, KC_VOLD, KC_MSEL, KC_VOLU),
+    MO(_FUNC), KC_0, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_LCTRL, KC_SPC,  KC_TRNS, KC_VOLD, KC_MSEL, KC_VOLU),
   /* \ has to go on here somehwere
    * insert home   up  end   pgup       ||      up     F7    F8    F9   F10
    *  del   left  down right pgdn       ||     down    F4    F5    F6   F11
@@ -50,7 +50,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_INS,  KC_HOME, KC_UP,   KC_END,  KC_PGUP,                   KC_UP,   KC_F7,   KC_F8,   KC_F9,   KC_F10  ,
     KC_DELT, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN,                   KC_DOWN, KC_F4,   KC_F5,   KC_F6,   KC_F11  ,
     KC_NO,   KC_VOLU, KC_NO,   KC_NO,   RESET,                     KC_NO,   KC_F1,   KC_F2,   KC_F3,   KC_F12  ,
-    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LCTL, KC_LALT, KC_SPC,  TO(_TYPING), KC_PSCR, KC_SLCK, KC_PAUS )
+    KC_NO,   KC_VOLD, KC_LGUI, KC_LSFT, KC_BSPC, KC_LALT, KC_LCTRL, KC_SPC,  TO(_TYPING), KC_PSCR, KC_SLCK, KC_PAUS )
 };
 
 const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
@@ -65,4 +65,4 @@ const macro_t *action_get_macro(keyrecord_t *record, uint8_t id, uint8_t opt) {
     break;
   }
   return MACRO_NONE;
-};
+}
